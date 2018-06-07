7b622df04369e0a56262ffd13d1ba238fd558a45
Tacombel <tacombel@gmail.com>
2018-06-07 07:14:57 +0000

    Deployment script improved

--- upgrade_app.sh
+++ upgrade_app.sh
@@ -1,9 +1,10 @@
 #bash
 #Se puede encontrar en el journal cuando se ejecuta este script con journalctl -t portfolio
 echo 'Actualizando porfolio' | systemd-cat -t portfolio -p info
-scp -r /home/daniel/proyectos/portfolio/app/ portfolio@192.168.1.55:/home/portfolio/portfolio
-scp -r /home/daniel/proyectos/portfolio/chromedriver/ portfolio@192.168.1.55:/home/portfolio/portfolio
-scp /home/daniel/proyectos/portfolio/config.py portfolio@192.168.1.55:/home/portfolio/portfolio
-scp /home/daniel/proyectos/portfolio/portfolio.py portfolio@192.168.1.55:/home/portfolio/portfolio
-scp /home/daniel/proyectos/portfolio/requirements.txt portfolio@192.168.1.55:/home/portfolio/portfolio
-scp /home/daniel/proyectos/portfolio/scrape.py portfolio@192.168.1.55:/home/portfolio/portfolio
+rsync --delete --progress --omit-dir-times --no-perms --stats -avz /home/daniel/proyectos/portfolio/app/ portfolio@192.168.1.55:/home/portfolio/portfolio/app/
+rsync --delete --progress --omit-dir-times --no-perms --stats -avz /home/daniel/proyectos/portfolio/chromedriver/ portfolio@192.168.1.55:/home/portfolio/portfolio/chromedriver/
+rsync --delete --progress --omit-dir-times --no-perms --stats -avz /home/daniel/proyectos/portfolio/migrations/ portfolio@192.168.1.55:/home/portfolio/portfolio/migrations/
+rsync --delete --progress --omit-dir-times --no-perms --stats -avz /home/daniel/proyectos/portfolio/config.py portfolio@192.168.1.55:/home/portfolio/portfolio/
+rsync --delete --progress --omit-dir-times --no-perms --stats -avz /home/daniel/proyectos/portfolio/portfolio.py portfolio@192.168.1.55:/home/portfolio/portfolio/
+rsync --delete --progress --omit-dir-times --no-perms --stats -avz /home/daniel/proyectos/portfolio/requirements.txt portfolio@192.168.1.55:/home/portfolio/portfolio/
+rsync --delete --progress --omit-dir-times --no-perms --stats -avz /home/daniel/proyectos/portfolio/scrape.py portfolio@192.168.1.55:/home/portfolio/portfolio/
