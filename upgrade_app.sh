d79c75af1001f134b8209524a3430d65100aea56
Tacombel <tacombel@gmail.com>
2018-06-18 16:24:54 +0200

    Update upgrade_app

--- upgrade_app.sh
+++ upgrade_app.sh
@@ -1,9 +1,9 @@
 #bash
 #Se puede encontrar en el journal cuando se ejecuta este script con journalctl -t portfolio
 echo 'Actualizando porfolio' | systemd-cat -t portfolio -p info
-scp /home/daniel/proyectos/portfolio/app/ portfolio@192.168.1.55:/home/portfolio/portfolio/app/
-scp /home/daniel/proyectos/portfolio/chromedriver/ portfolio@192.168.1.55:/home/portfolio/portfolio/chromedriver/
-scp /home/daniel/proyectos/portfolio/migrations/ portfolio@192.168.1.55:/home/portfolio/portfolio/migrations/
+scp -r /home/daniel/proyectos/portfolio/app/ portfolio@192.168.1.55:/home/portfolio/portfolio/app/
+scp -r /home/daniel/proyectos/portfolio/chromedriver/ portfolio@192.168.1.55:/home/portfolio/portfolio/chromedriver/
+scp -r /home/daniel/proyectos/portfolio/migrations/ portfolio@192.168.1.55:/home/portfolio/portfolio/migrations/
 scp /home/daniel/proyectos/portfolio/config.py portfolio@192.168.1.55:/home/portfolio/portfolio/
 scp /home/daniel/proyectos/portfolio/portfolio.py portfolio@192.168.1.55:/home/portfolio/portfolio/
 scp /home/daniel/proyectos/portfolio/requirements.txt portfolio@192.168.1.55:/home/portfolio/portfolio/
